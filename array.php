cb3cbbc0dde24eb170b5dd1bad01b5afd4cbeaf5
prajapatianil77 <anilprajapatbkbiet2022@gmail.com>
2025-04-15 15:32:48 +0530

    Array in php

---
+++ array.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Array in php</title>
+   
+</head>
+<body>
+    <?php
+    // $ar = array("Jaipur","Jhunjhunu","Delhi","Mumbai","Churu");  
+    // // print_r($ar);
+    
+    // for($i=0;$i<count($ar);$i++){
+    //     echo $ar[$i];
+    //     echo "<br>";
+    // }
+
+    $arr = ["a"=>"Jaipur","b"=>"Jhunjhunu","c"=>"Delhi"];    // this is associative array type
+    print_r($arr);
+    ?>
+</body>
+</html>
